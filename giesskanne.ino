0e3274036945fc5c430b0d45b00c1c1ce317d544
Christoph Steinbeck <er@doktor-steinbeck.de>
2020-09-14 15:53:36 +0200

    Update giesskanne.ino
    Migrated to ESP32 LoRa board with OLED display.

--- giesskanne.ino
+++ giesskanne.ino
@@ -1,16 +1,38 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+#define ss 18
+#define rst 14
+#define dio0 26
+
+
+# Unusable Pins on this device due to LoRa and OLED modules
+# Used by LoRa:
+# SCK  5
+# MISO  19
+# MOSI  27
+# CS  18
+# RST 14
+# DIO0  26
+# Used by OLED Display > ESP32 (I2C)
+# SCL 15
+# SDA 4
+# RST 16
+
+
 
 // Set number of relays
 #define NUM_ZONES  6
-#define NUM_MINUTES 1
+#define NUM_MINUTES 3
 
 // Assign each GPIO to a relay
-int relayGPIOs[NUM_ZONES] = {16, 17, 18, 19, 21, 22};
+int relayGPIOs[NUM_ZONES] = {0, 22, 23, 18, 15, 17};
 
 boolean one_run_done = false;
 int zone = 0;
 long lastmillis = 0;
 long currentmillis = 0;
-long irrigationtime = NUM_MINUTES * 60000; //this should be 60000 for minutes. 
+long irrigationtime = NUM_MINUTES * 1000; //this should be 60000 for minutes. 
 
 void setup() {
   Serial.begin(115200);
