2f81573678aa8cc1205bc662623ba5dad3142d12
Christoph Steinbeck <er@doktor-steinbeck.de>
2020-09-21 17:36:06 +0200

    Update giesskanne.ino

--- giesskanne.ino
+++ giesskanne.ino
@@ -1,14 +1,14 @@
 #include <SPI.h>
 #include <LoRa.h>
 #include <U8x8lib.h>
+#include "Ticker.h" // This is https://github.com/sstaub/Ticker
+
 
 #define ss 18
 #define rst 14
 #define dio0 26
 
 U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(15, 4, 16);
-
-
 
 //# Unusable Pins on this device due to LoRa and OLED modules
 //# Used by LoRa:
@@ -23,105 +23,86 @@ U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(15, 4, 16);
 //# SDA 4
 //# RST 16
 
-int irrigationTime = 30;
-
 // Set number of relays
 #define NUM_ZONES  6
-#define NUM_MINUTES 3
+
+int  num_minutes=1;
 
 // Assign each GPIO to a relay
 int relayGPIOs[NUM_ZONES] = {33, 22, 23, 2, 32, 17};
 const int plusbutton = 36;
 const int minusbutton = 37;
-const int triggerbutton = 38;
+const int triggerbutton = 39;
 
 int minusbuttonState = 0;
 int plusbuttonState = 0;
 int triggerbuttonState = 0;
 
 boolean one_run_done = false;
-boolean irrigationTimeChanged = false;
 boolean paused = true;
+boolean start = false;
 int zone = 0;
 long lastmillis = 0;
 long currentmillis = 0;
-long irrigationtime = NUM_MINUTES * 1000; //this should be 60000 for minutes. 
+long irrigationtime = num_minutes * 60000; //this should be 60000 for minutes. 
+
+void updateTimeLeft();
+Ticker updateTimer(updateTimeLeft, 1000, 0, MILLIS);
 
 void setup() {
   Serial.begin(115200);
+
+   /* 
+   * Initialize OLED display and print App name plus version
+   */
   u8x8.begin(); 
   u8x8.setFont(u8x8_font_chroma48medium8_r);
   u8x8.setFlipMode(1);
-  u8x8.drawString(0, 1, "Giesskanne 1.0");
-  pinMode(36, INPUT);
-  pinMode(37, INPUT);
- 
+  u8x8.drawString(0, 0, "Giesskanne 1.0");
+  /* 
+   * Set all GPIO triggering relays to OUTPUT  
+   */
+
+  pinMode(plusbutton, INPUT);
+  pinMode(minusbutton, INPUT);
+  pinMode(triggerbutton, INPUT);
+  /* 
+   * Set all GPIO triggering relays to OUTPUT  
+   */
   for(int i = 0; i < NUM_ZONES; i++){
     pinMode(relayGPIOs[i], OUTPUT);
     digitalWrite(relayGPIOs[i], HIGH);
   }
-
-  Serial.println("Starting irrigation schedule ....");
-  Serial.print("Irrigation time set to ");
-  Serial.print(irrigationtime);
-  Serial.println(" milliseconds.");
-
-  
-  digitalWrite(relayGPIOs[zone], LOW);
-  Serial.print("Switched on zone ");
-  Serial.println(zone+1);
-  zone++;
-  lastmillis = millis();
-  
+  u8x8.setCursor(0, 3);
+  u8x8.print("IrrigT = ");
+  u8x8.print(num_minutes);
+  u8x8.print(" min");
+  start = true;  
+  updateTimer.start();
 }
 
 void loop() {
-
-//  Serial.print("Plus Botton: ");
-//  Serial.println(plusbuttonState);
-//  Serial.print("Minus Botton: ");
-//  Serial.println(minusbuttonState);
+  irrigationtime = num_minutes * 60000; //this should be 60000 for minutes. 
+  updateTimer.update();
 
   processButtons();
-  if (irrigationTimeChanged) 
-  {
-    u8x8.setCursor(0, 4);
-    u8x8.print("T = ");
-    u8x8.print(irrigationTime);
-    u8x8.print(" min");
-    irrigationTimeChanged = false;
-    
-  }
 
   if (paused) 
   {
-    u8x8.setCursor(0, 16);
-    u8x8.print("Paused");
+    u8x8.setCursor(0, 1);
+    u8x8.print("Paused     ");
   }
-  else
+  if (!paused)
   {
-    u8x8.setCursor(0, 16);
-    u8x8.print("Running");
-    
+    u8x8.setCursor(0, 1);
+    u8x8.print("Running    ");
   }
   
   if (!one_run_done && !paused)
   {
     currentmillis = millis();
-    if (currentmillis%1000 == 0)
-    { 
-      //Serial.println(currentmillis - lastmillis);
-      //Serial.println(irrigationtime);
-    }
-    if ((currentmillis - lastmillis)%60000 == 0)
-    { 
-          u8x8.setCursor(0, 50);
-          u8x8.print((currentmillis - lastmillis)/60000);
-          u8x8.print(" m left in z ");
-          u8x8.print(zone);
-    }
     
-    if ((currentmillis - lastmillis) > irrigationtime)
+    if ((currentmillis - lastmillis) > irrigationtime || start)
     {
       if (zone < NUM_ZONES)
       {
@@ -138,9 +119,13 @@ void loop() {
       }
       zone++;
       lastmillis = millis();
+      start = false;
       if (zone == NUM_ZONES + 1) 
       {
         one_run_done = true;
+        paused = true;
+        u8x8.setCursor(0, 5);
+        u8x8.print("Finished!");
         Serial.println("Irrigation schedule finished");
       }
     }
@@ -151,32 +136,48 @@ void loop() {
   void processButtons()
   {
     plusbuttonState = digitalRead(plusbutton);
-    //Serial.println(plusbuttonState);
     minusbuttonState = digitalRead(minusbutton);
-    //Serial.println(minusbuttonState);
     triggerbuttonState = digitalRead(triggerbutton);
-    Serial.println(triggerbuttonState);
     if (plusbuttonState == 1) 
     {
-      irrigationTime ++;
-      irrigationTimeChanged = true;
+      num_minutes ++;
+      irrigationTimeChanged();
       delay(200);
       return;
     }
-    if (minusbuttonState == 1  && irrigationTime > 1){
-      irrigationTime --; 
-      irrigationTimeChanged = true;  
+    if (minusbuttonState == 1  && num_minutes > 1){
+      num_minutes --; 
+      irrigationTimeChanged();  
       delay(200);
       return;
     }
     if (triggerbuttonState == 1) 
     {
-      
       if (paused) paused = false;
-      if (!paused) paused = true;
+      else paused = true;
       delay(200);
       return;
     }
-    
-    
   }
+
+  void updateTimeLeft()
+  {
+    if (paused) return;
+    Serial.print(".");
+    u8x8.setCursor(0, 4);
+    u8x8.print(((currentmillis - lastmillis))/60000);
+    u8x8.print("/");
+    u8x8.print(num_minutes);
+    u8x8.print("min in Z");
+    u8x8.print(zone);
+  }    
+
+  void irrigationTimeChanged()
+  {
+    u8x8.setCursor(0, 3);
+    u8x8.print("IrrigT = ");
+    u8x8.print(num_minutes);
+    u8x8.print(" min");
+  }
+
+  
